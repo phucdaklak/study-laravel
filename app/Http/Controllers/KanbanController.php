82b85abdf536deac0c7db56e529766001e99884b
phucdaklak <dotanphuc9494@gmail.com>
2023-12-21 07:31:45 +0700

    add kanban controller

---
+++ app/Http/Controllers/KanbanController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class KanbanController extends Controller
+{
+    public function index(Request $request)
+    {
+        return 'home-page';
+    }
+
+    public function store(Request $request)
+    {
+      
+    }
+
+    public function update(Request $request)
+    {
+      
+    }
+
+    public function show(Request $request)
+    {
+      
+    }
+
+    public function destroy(Request $request)
+    {
+      
+    }
+}
